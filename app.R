e3eaf285dfb6b174ffaa5c5589a6c7c335fdb144
Claire <clrccljuhasz@gmail.com>
2023-10-02 17:05:54 -0400

    Layout in dev

--- app.R
+++ app.R
@@ -2,30 +2,52 @@ library(shiny)
 library(shinydashboard)
 library(leaflet)
 library(sf)
-library(osmdata)
-library(mapview)
 library(htmltools)
 library(gdalcubes)
 library(rstac)
+library(terra)
 
 
 
 species <- list.files("/home/claire/BDQC-GEOBON/data/Bellavance_data/original_maps")
-queb <-
-  ui <- fluidPage(
-    sidebarLayout(
-      sidebarPanel(
-        selectInput("maps_Vincent", label = "SDMs Vincent", choices = species)
+
+
+# occs <-
+queb <- st_read("/home/claire/BDQC-GEOBON/GITHUB/BDQC_SDM_benchmark_initial/local_data/QC_region_raster_Vince.gpkg")
+
+ui <- dashboardPage(
+  dashboardHeader(title = "Explorateur de SDMs"),
+  dashboardSidebar(width = "0px"),
+  dashboardBody(
+    # First row
+    fluidRow(
+      box(
+        title = "choix",
+        width = 4,
+        selectInput("maps_Vincent",
+          label = "SDMs Vincent",
+          choices = species
+        )
       ),
-      mainPanel(
+      box(
+        title = "carte e-bird",
+        width = 8
+      )
+    ),
+    # Second row
+    fluidRow(
+      box(
+        title = "carte INLA - auto-corrélation spatiale",
+        width = 6,
         plotOutput("map_Vince")
+      ),
+      box(
+        title = "carte mapSPecies",
+        width = 6
       )
     )
   )
-
-
-
-
+)
 
 
 server <- function(input, output, session) {
@@ -45,7 +67,29 @@ server <- function(input, output, session) {
     go_cat <- stars::read_stars(paste0("/vsicurl/", tif_path),
       proxy = TRUE
     )
-    plot(go_cat)
+    plot(go_cat,
+      axes = TRUE,
+      main = "",
+      col = c("#f6f8e0", "#009999"),
+      legend = FALSE
+    )
+
+    legend("topright",
+      fill = c("#f6f8e0", "#009999"),
+      border = "black",
+      legend = c("absente", "présente"),
+      bty = "n"
+    )
   })
+
+
+  plot(st_geometry(queb), axes = T, add = T)
 }
 shinyApp(ui, server)
+
+
+
+# aegolius_funereus
+# aegolius_acadicus
+# asia otus
+# tympanuchus_phasianellus
